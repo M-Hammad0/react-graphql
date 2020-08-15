f930a70076dbf1b2467145c63327a6f5e7d777df
M-Hammad0 <hammad_205@hotmail.com>
2020-08-15 11:33:33 +0500

    add LaucnhList component

---
+++ src/Components/LaunchList/LaunchList.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import Loading from "../Loading/Loading";
+import { Waypoint } from "react-waypoint";
+
+interface LaunchListI {
+  id: string,
+  launch_date_local: string;
+  launch_success: boolean;
+  mission_name: string;
+}
+
+interface LaunchListData {
+  launches: LaunchListI[];
+}
+
+interface LaunchListVars {
+  offset: number;
+  limit: number;
+}
+
+const GET_LAUNCH_INFO = gql`
+  query Launches($limit: Int!, $offset: Int!) {
+    launches(limit: $limit, offset: $offset) {
+      id
+      launch_date_local
+      launch_success
+      mission_name
+    }
+  }
+`;
+
+function LaunchList() {
+  const { loading,data, fetchMore, networkStatus} = useQuery<LaunchListData, LaunchListVars>(
+    GET_LAUNCH_INFO,
+    {
+      variables: {
+        offset: 0,
+        limit: 25,
+      },
+      notifyOnNetworkStatusChange: true,
+    },
+  );
+
+  if (!data?.launches && loading) return <Loading />
+  return (
+    <div>
+      <h3>Launches</h3>
+      <div>
+        {data?.launches.map((launched, i) => (
+            <div key={i}>
+              <p
+                className="box"
+                style={{ color: launched.launch_success ? "green" : "red" }}
+              >
+                {launched.mission_name}
+              </p>
+              {i === data.launches.length - 5 && (
+                <Waypoint onEnter={() =>
+                  fetchMore({
+                    variables: {
+                      offset: data?.launches.length,
+                    },
+                    updateQuery: (prev: any, {fetchMoreResult} ) => {
+                      if(!fetchMoreResult) return prev;
+                      return {
+                        ...prev,
+                        launches: [...prev.launches,...fetchMoreResult.launches]
+                      }
+                    },
+                  })
+
+                } />
+              )}
+            </div>
+          ))}
+      </div>
+      {networkStatus === 3 && <h1>loading...</h1>}
+    </div>
+  );
+}
+
+export default LaunchList;
