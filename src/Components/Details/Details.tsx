6430a52229f1d76b555713446cf3bfd063a6cf28
M-Hammad0 <hammad_205@hotmail.com>
2020-08-22 02:21:57 +0500

    add styles to details component

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router";
 import { CircularProgress } from "@material-ui/core";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Table from "react-bootstrap/Table";
 
 interface DetailsI {
   id: string;
   mission_name: string;
   launch_date_local: string;
   launch_success: boolean;
-  details: string,
+  details: string;
+  launch_site: {
+    site_name_long: string;
+  };
+  rocket: {
+    rocket_name: string;
+    rocket_type: string;
+  };
   links: {
     flickr_images: Array<String>;
     article_link: Object;
@@ -29,7 +41,15 @@ export const GET_LAUNCH_DETAILS = gql`
       id
       mission_name
       launch_success
+      launch_date_local
       details
+      launch_site {
+        site_name_long
+      }
+      rocket {
+        rocket_name
+        rocket_type
+      }
       links {
         article_link
         flickr_images
@@ -54,18 +74,51 @@ function Details() {
 
   return (
     <div>
-      <h1 data-testid="launch-detail">
-        {data?.launches.map((i) => (
-          <div key={i.id}>
-          <h3>{i.mission_name}</h3>
-          <p>{i.details}</p>
-          {/* <img style={{width: "50%"}} src={i.links.flickr_images[0].toString()} alt="" /> */}
-          {i.links.flickr_images ? i.links.flickr_images.map((img,idx) => (
-            <img key={idx} style={{width: "20%"}} src={img.toString()} alt="" />
-          )): null}
-          </div>
-        ))}
-      </h1>
+      <Container>
+        <Row>
+          <Col>
+            {data?.launches.map((i) => (
+              <div key={i.id}>
+                <h3>{i.mission_name}</h3>
+                <p style={{margin: "5% 0"}}>{i.details}<a href={i.links.article_link ? i.links.article_link.toString(): ""}> read more</a></p>
+                <Table  bordered hover variant="dark">
+                  <tbody>
+                    <tr>
+                      <td>Launch Success</td>
+                      <td style={{backgroundColor: i.launch_success ? "green" : "red"}}>{i.launch_success}</td>
+                    </tr>
+                    <tr>
+                      <td>Launch Date</td>
+                      <td>{i.launch_date_local}</td>
+                    </tr>
+                    <tr>
+                      <td>Launch Site</td>
+                      <td>{i.launch_site.site_name_long}</td>
+                    </tr>
+                    <tr>
+                      <td>Rocket Name</td>
+                      <td>{i.rocket.rocket_name}</td>
+                    </tr>
+                    <tr>
+                      <td>Rocket Type</td>
+                      <td>{i.rocket.rocket_type}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            ))}
+          </Col>
+          <Col lg={6}>
+            {data?.launches.map((img, i) => (
+              <div key={i}>
+                {img.links.flickr_images[0] ? (
+                  <Image src={img.links.flickr_images[0].toString()} fluid />
+                ) : <p>No images available</p>}
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
