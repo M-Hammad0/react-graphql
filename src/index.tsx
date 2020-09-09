48817ffaf94afc0d203d8d18568f88c021297878
unknown <hammad_205@hotmail.com>
2020-09-10 02:06:22 +0500

    fixed URI

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import {
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: "http://api.spacex.land/graphql/",
+    uri: "https://api.spacex.land/graphql/",
   }),
 });
 
